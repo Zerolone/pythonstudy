398be015e87dca0a08ff4a164ea6fa15b456d9d3
Zerolone <Zerolone@163.com>
2017-10-06 11:15:02 +0800

    004

--- 005.py
+++ 005.py
@@ -2,7 +2,7 @@
 # coding: UTF-8
 
 '''
-regex
+regex match 与 search区别
 '''
 
 import re
@@ -14,15 +14,54 @@ a = re.match('com', 'www.zerolone.com').span();
 #print a;
 '''
 
-line = "Cats are smarter than dogs"
-matchObj = re.match ( r'(.*) are (.*?) .*', line, re.M|re.I)
-matchObj = re.search( r'(.*) are (.*?) .*', line, re.M|re.I)
+line = '''
+Cats are smarter1 than dogs
+Cats are smarter2 than dogs
+Cats are smarter3 than dogs
+'''
+matchObj = re.match ( r'are (.*?) .*', line, re.M|re.I)
+if matchObj:
+  print matchObj.group();
+  print matchObj.group(1);
+else:
+  print 'no match'
+
+
+
+matchObj = re.search( r'are (.*?) .*', line, re.M|re.I)
 
 #print matchObj;
 
 if matchObj:
   print matchObj.group();
   print matchObj.group(1);
-  print matchObj.group(2);
+else:
+  print 'no match'
 
 
+
+
+matchObj = re.search( r'are (.*?) .*', line, re.M|re.I)
+
+#print matchObj;
+
+if matchObj:
+  print matchObj.group();
+  print matchObj.group(1);
+else:
+  print 'no match'
+
+
+line = '''
+Cats are smarter1 than dogs
+Cats are smarter2 than dogs
+Cats are smarter3 than dogs
+'''
+matchObj = re.findall( r'are (.*?) than (.*)', line, re.M|re.I)
+
+print matchObj;
+
+
+matchObj = re.finditer( r'are (.*?) .*', line, re.M|re.I)
+
+print matchObj;
---
+++ 012.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+
+import platform;
+
+#print platform.uname();
+
+#print platform._release_filename
+
+
+a = [1,2,3,4,5];
+a*=2;
+
+print a;
